b920ce10b2d666dfe08ccb0524a27f50c3050828
wywang <727936199@qq.com>
2020-11-01 21:37:39 +0800

    给iconPosition添加默认值left，以及检测iconPosition值只能为left和right

--- src/button.vue
+++ src/button.vue
@@ -1,41 +1,53 @@
 <template>
-    <div>
-        <button class="m-button">
-            <Icon v-if="icon&&iconPosition!==`right`" :icon="icon"></Icon>
-            <slot></slot>
-            <Icon v-if="icon&&iconPosition===`right`" :icon="icon"></Icon>
-        </button>
-    </div>
+    <button class="m-button">
+        <Icon v-if="icon&&iconPosition===`left`" :icon="icon"></Icon>
+        <slot></slot>
+        <Icon v-if="icon&&iconPosition===`right`" :icon="icon"></Icon>
+    </button>
 </template>
 
 <script>
     import Icon from './components/Icon';
 
-    export default{
-        props:['icon','iconPosition'],
+    export default {
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator: (value) => {
+                  if(value!=='left'&&value!=='right'){
+                      return false;
+                  }else{
+                      return true;
+                  }
+                }
+
+            }
+        },
         components: {Icon}
-    }
+    };
 </script>
 
 <style lang="scss" scoped>
 
-    .m-button{
+    .m-button {
         height: var(--button-height);
         padding: 0 1em;
         background-color: var(--button-bg);
         border: 1px solid var(--border-color);
         border-radius: var(--border-radius);
 
-        &:active{
+        &:active {
             background-color: var(--button-active-bg);
         }
 
-        &:hover{
-            border-color:var(--border-color-hover);
+        &:hover {
+            border-color: var(--border-color-hover);
         }
 
-        &:focus{
-            outline:none;
+        &:focus {
+            outline: none;
         }
     }
 

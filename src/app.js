f85ac8ab7843477fc126ab7bb8f4f36bc08aac85
wywang <727936199@qq.com>
2020-11-08 20:06:18 +0800

    新建Row和Col组件，并完成使用span参数决定row中col的宽度

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 
 Vue.component('m-button',Button)
 Vue.component('m-icon',Icon)
 Vue.component('m-button-group',ButtonGroup)
 Vue.component('m-input',Input)
+Vue.component('m-row',Row)
+Vue.component('m-col',Col)
 
 new Vue({
     el:'#app',

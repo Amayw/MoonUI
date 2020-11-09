0f8401cd1a6122032968468ba8e947907626cde2
wywang <727936199@qq.com>
2020-11-09 14:34:57 +0800

    给Row组件添加gutter参数，从而通过gutter给Col之间添加默认间隙。利用mounted()函数，将Row中的gutter值传给Col

---
+++ src/col.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="col" :class="[span&&`col-${span}`,offset&&`offset-${offset}`]"
+         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+        <div class="inner">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'MoonCol',
+        props:{
+            span:{
+                type:[String,Number]
+            },
+            offset:{
+                type:[String,Number]
+            }
+        },
+        data(){
+            return{
+                gutter:0
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .col{
+        height: 100px;
+        width: 100%;
+        >.inner{
+            border: 1px solid red;
+        }
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n / 24)*100%;
+            }
+        }
+
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24)*100%;
+            }
+        }
+
+    }
+
+</style>

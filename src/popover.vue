6efa926e3262be20f1a06228a516156a8a7c6199
wywang <727936199@qq.com>
2020-12-09 13:49:41 +0800

    初步实现popover

---
+++ src/popover.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="moon-popover" @click="changeVisible">
+        <div  v-if="visible" class="moon-content-wrapper">
+            <slot name="content"></slot>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'MoonPopOver',
+        data(){
+            return{
+                visible:false
+            }
+        },
+        methods:{
+            changeVisible(){
+                console.log(1);
+                this.visible=!this.visible;
+                console.log(this.visible);
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .moon-popover{
+        display: inline-flex;
+        vertical-align: top;
+        position: relative;
+        .moon-content-wrapper{
+            position: absolute;
+            bottom: 100%;
+            border: 1px solid pink;
+            box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        }
+    }
+</style>

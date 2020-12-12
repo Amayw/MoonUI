15d0112f4e3412453fb9597a6197dc0951dd2826
wywang <727936199@qq.com>
2020-12-12 10:37:37 +0800

    collapse基本样式和基本功能完成

---
+++ src/collapse-item.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="m-collapse-item">
+        <div class="m-title"  @click="open=!open">
+            <m-icon icon="right"></m-icon>
+            <span>{{title}}</span>
+        </div>
+        <div v-if="open" class="m-content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"MoonCollapseItem",
+        props:{
+            title:{
+                type:String,
+                required:true
+            }
+        },
+        data(){
+            return {
+                open:false
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .m-collapse-item{
+        .m-title{
+            border: 1px solid #ddd;
+            margin: -1px -1px 0 -1px;
+            min-height: 32px;
+            padding: 0 8px;
+            display: flex;
+            align-items: center;
+        }
+        &:first-child{
+            >.m-title{
+                border-top-left-radius: 4px;
+                border-top-right-radius: 4px;
+            }
+        }
+        &:last-child{
+            >.m-title:last-child{
+                border-bottom-left-radius: 4px;
+                border-bottom-right-radius: 4px;
+                border-bottom: none;
+            }
+        }
+        >.m-content{
+            padding: 8px;
+        }
+    }
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="m-collapse">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"MoonCollapse"
+    };
+</script>
+
+<style lang="scss" scoped>
+    .m-collapse{
+        border: 1px solid #ddd;
+        border-radius: 4px;
+    }
+</style>
